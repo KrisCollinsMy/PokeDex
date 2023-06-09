2cfcc1076e17f2ae0fe5af72b01ae977539c9c85
kris collins <kriscollinsmy@gmail.com>
2023-06-09 16:19:57 -0500

    [perf] Add text compression and javascript minification

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { terser } from 'rollup-plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'happy-dom'
+  },
+  build: {
+    rollupOptions: {
+      plugins: [terser(), gzipPlugin()]
+    },
   },
 })
